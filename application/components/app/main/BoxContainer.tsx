1f65e48743e100fb523b5e2c9779fde18d62f824
Aissa Bedr <slayaissabedr@gmail.com>
2023-04-02 00:35:25 +0100

    update the main folder

--- application/components/app/main/BoxContainer.tsx
+++ application/components/app/main/BoxContainer.tsx
@@ -1,17 +1,17 @@
-import { AppState, SwitchBooleans } from "@/redux/types/main";
+import { AppState } from "@/redux/types/main";
 import classNames from "classnames";
 import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { BoxContainerProps } from "./types/main";
 
 const BoxContainer: FC<BoxContainerProps> = ({ className = "", dontIncludeSpaces, children }) => {
-    const switchBooleans = useSelector<AppState, SwitchBooleans>((state) => state.switchBooleans);
+    const state = useSelector<AppState, AppState>((state) => state);
 
     return (
         <div
             className={classNames("__box_container", {
                 [`${className}`]: className,
-                "rounded-md": switchBooleans.uiControl.isRounded,
+                "rounded-md": state.switchBooleans.uiControl.isRounded,
                 "p-5": !dontIncludeSpaces,
             })}
         >
