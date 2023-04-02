ba707d0a3e66f48c1f3c4a86ebef70ad8d267437
Aissa Bedr <slayaissabedr@gmail.com>
2023-04-02 01:02:30 +0100

    update the search bar component

--- application/components/helpers/search_bar/SearchBar.tsx
+++ application/components/helpers/search_bar/SearchBar.tsx
@@ -1,22 +1,22 @@
 import Flex from "@/components/build/Flex";
-import { AppState, SwitchBooleans } from "@/redux/types/main";
+import { AppState } from "@/redux/types/main";
 import classNames from "classnames";
 import React from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { useSelector } from "react-redux";
 
 const SearchBar = () => {
-    const switchBooleans = useSelector<AppState, SwitchBooleans>((state) => state.switchBooleans);
+    const state = useSelector<AppState, AppState>((state) => state);
 
     return (
         <>
-            {switchBooleans.uiControl.isSearchBarShowed && (
+            {state.switchBooleans.uiControl.isSearchBarShowed && (
                 <Flex
                     className={classNames(
                         "gap-2 py-2 px-4 bg-white text-grey-color dark:bg-dark w-full dark:text-grey-dark-color rounded-full cursor-pointer",
                         {
-                            "mb-2": switchBooleans.websiteControl.isZoomIn,
-                            "my-2 lg:mb-2 lg:mt-0": !switchBooleans.websiteControl.isZoomIn,
+                            "mb-2": state.switchBooleans.websiteControl.isZoomIn,
+                            "my-2 lg:mb-2 lg:mt-0": !state.switchBooleans.websiteControl.isZoomIn,
                         }
                     )}
                     direction="row"
