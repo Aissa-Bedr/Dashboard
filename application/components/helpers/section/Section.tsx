f96a1ba7dc862e302fc14782c49e84b41d1bb199
Aissa Bedr <slayaissabedr@gmail.com>
2023-04-02 01:04:00 +0100

    update the section component

--- application/components/helpers/section/Section.tsx
+++ application/components/helpers/section/Section.tsx
@@ -1,7 +1,7 @@
 import Flex from "@/components/build/Flex";
-import { AppState, AppStateAction, SwitchBooleans } from "@/redux/types/main";
+import { AppState, AppStateAction } from "@/redux/types/main";
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { BiBell, BiSearchAlt2 } from "react-icons/bi";
 import { HiArrowSmRight, HiMenuAlt1, HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,12 +10,9 @@ import SecButton from "./SecButton";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import classNames from "classnames";
-import Button from "@/components/build/Button";
-import Input from "@/components/build/Input";
 
 const Section = () => {
     const state = useSelector<AppState, AppState>((state) => state);
-    const switchBooleans = useSelector<AppState, SwitchBooleans>((state) => state.switchBooleans);
     const dispatch: Dispatch<AppStateAction> = useDispatch();
 
     return (
@@ -23,8 +20,8 @@ const Section = () => {
             <Flex
                 className={classNames("w-full p-4 duration-300 bg-white lg:px-4 lg:py-0 dark:bg-dark", {
                     "fixed top-0 left-0 z-40 shadow-md shadow-[#ddd] dark:shadow-grey-dark-alt-color":
-                        switchBooleans.uiControl.isNavFixed,
-                    "relative shadow-none": !switchBooleans.uiControl.isNavFixed,
+                        state.switchBooleans.uiControl.isNavFixed,
+                    "relative shadow-none": !state.switchBooleans.uiControl.isNavFixed,
                 })}
                 direction="row"
                 items="center"
@@ -35,17 +32,17 @@ const Section = () => {
                         <div
                             className={classNames("absolute hidden lg:block", {
                                 "left-16":
-                                    switchBooleans.uiControl.isNavFixed &&
-                                    switchBooleans.uiControl.isNavMinimized &&
-                                    !switchBooleans.uiControl.isNewNavbar,
+                                    state.switchBooleans.uiControl.isNavFixed &&
+                                    state.switchBooleans.uiControl.isNavMinimized &&
+                                    !state.switchBooleans.uiControl.isNewNavbar,
                                 "left-56":
-                                    switchBooleans.uiControl.isNavFixed &&
-                                    !switchBooleans.uiControl.isNavMinimized &&
-                                    !switchBooleans.uiControl.isNewNavbar,
+                                    state.switchBooleans.uiControl.isNavFixed &&
+                                    !state.switchBooleans.uiControl.isNavMinimized &&
+                                    !state.switchBooleans.uiControl.isNewNavbar,
                             })}
                         >
                             <SecButton onClick={() => dispatch({ type: "toggleIsNavMinimized" })}>
-                                {switchBooleans.uiControl.isNavMinimized ? (
+                                {state.switchBooleans.uiControl.isNavMinimized ? (
                                     <HiArrowSmRight size="1.5rem" />
                                 ) : (
                                     <HiMenuAlt1 size="1.5rem" />
@@ -57,7 +54,7 @@ const Section = () => {
 
                 <Flex className="gap-2" direction="row" items="center">
                     <SecButton
-                        className={classNames({ "!hidden": !switchBooleans.uiControl.isNewSearchbar })}
+                        className={classNames({ "!hidden": !state.switchBooleans.uiControl.isNewSearchbar })}
                         onClick={() => dispatch({ type: "toggleIsSearchBarShowed" })}
                     >
                         <BiSearchAlt2 size="1.5rem" />
@@ -66,7 +63,7 @@ const Section = () => {
                     <SecButton
                         onClick={() => {
                             dispatch({ type: "toggleTheme" });
-                            switchBooleans.websiteControl.isNotificationActive &&
+                            state.switchBooleans.websiteControl.isNotificationActive &&
                                 toast.success(
                                     `Theme changed successfully current theme: ${
                                         state.theme === "dark" ? "light" : "dark"
