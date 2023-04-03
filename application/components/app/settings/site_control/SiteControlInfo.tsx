f43c54385eeb268e68588423f526124347819fca
Aissa Bedr <slayaissabedr@gmail.com>
2023-04-03 07:07:58 +0100

    update the website control section

--- application/components/app/settings/site_control/SiteControlInfo.tsx
+++ application/components/app/settings/site_control/SiteControlInfo.tsx
@@ -6,23 +6,35 @@ import WebsiteControl from "./controls/WebsiteControl";
 import WidgetsControl from "./controls/WidgetsControl";
 import Details from "@/components/build/Details";
 import Flex from "@/components/build/Flex";
+import { useSelector } from "react-redux";
+import { AppState } from "@/redux/types/main";
 
 const SiteControlInfo = () => {
+    const state = useSelector<AppState, AppState>((state) => state);
+
     return (
         <Grid cols="1">
-            <Details className="mb-4" note="Important note">
-                <Flex className="gap-2 p-2" direction="col">
-                    <p className="text-sm __secondary_logo">
-                        The auto-select will be apply only in the current color theme and will effect the appearance
-                        control themes and components shapes control
-                    </p>
+            {state.subscription.subscribeType === "super" ||
+                (state.subscription.subscribeType === "max" && (
+                    <Details className="mb-4" note="Important note">
+                        <Flex className="gap-2 p-2" direction="col">
+                            <p className="text-sm __secondary_logo">
+                                The auto-select will be apply only in the current color theme and will effect the
+                                appearance control themes and components shapes control
+                            </p>
 
-                    <p className="text-sm __secondary_logo">
-                        When auto-select is enabled, you will not be able to edit anything that has been changed
-                        automatically
-                    </p>
-                </Flex>
-            </Details>
+                            <p className="text-sm __secondary_logo">
+                                When auto-select is enabled, you will not be able to edit anything that has been changed
+                                automatically
+                            </p>
+
+                            <p className="text-sm __secondary_logo">
+                                When auto-select is enabled in light theme, when you change the theme to dark it will
+                                automatically disabled and the opposite is true
+                            </p>
+                        </Flex>
+                    </Details>
+                ))}
 
             <ControlsItemContainer dontIncludePadTop>
                 <UIControl />
