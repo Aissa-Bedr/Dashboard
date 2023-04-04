421709a370ae088fc9f9d44a6bbcd241d0eef199
Aissa Bedr <slayaissabedr@gmail.com>
2023-04-04 01:02:41 +0100

    update the state store

--- application/redux/store.ts
+++ application/redux/store.ts
@@ -2,14 +2,13 @@ import { AppState } from "@/redux/types/main";
 import { legacy_createStore } from "redux";
 import { initialState } from "./state";
 import reducer from "./reducer";
-import { devToolsEnhancer, EnhancerOptions } from "@redux-devtools/extension";
 
 const appState: AppState =
     typeof window !== "undefined" && localStorage.getItem("appState")
         ? JSON.parse(`${localStorage.getItem("appState")}`)
         : initialState;
 
-const store = legacy_createStore(reducer, devToolsEnhancer(appState as EnhancerOptions));
+const store = legacy_createStore(reducer, appState);
 
 store.subscribe(() => {
     localStorage.setItem("appState", JSON.stringify(store.getState()));
